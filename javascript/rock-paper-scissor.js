2a574f392f8768dd7aa76cc0fb60f5dd58c463f4
naveedahmed-intp <dc230400318nak@vu.edu.pk>
2024-09-07 11:21:55 +0500

    Modified rock-paper-scissor.js file and Added playRound function along with two supporting functions getHumanChoice and getComputerChoice

--- javascript/rock-paper-scissor.js
+++ javascript/rock-paper-scissor.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     let rand = Math.floor(Math.random()*3) + 1;
     if (rand == 1) {
@@ -39,3 +42,43 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+        alert("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("Computer win! Paper beats Rock");
+        alert("Computer win! Paper beats Rock");
+        computerScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissor") {
+        console.log("You win! Rock beats Scissor");
+        alert("You win! Rock beats Scissor");
+        humanScore++;
+    }
+    else if (humanChoice == "scissor" && computerChoice == "rock") {
+        console.log("Computer win! Rock beats Scissor");
+        alert("Computer win! Rock beats Scissor");
+        computerScore++;
+    }
+    else if (humanChoice == "scissor" && computerChoice == "paper") {
+        console.log("You win! Scissor beats Paper");
+        alert("You win! Scissor beats Paper");
+        humanScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissor") {
+        console.log("Computer win! Scissor beats Paper");
+        alert("Computer win! Scissor beats Paper");
+        computerScore++;
+    }
+    else {
+        console.log("Tie, play again");
+        alert("Tie, play again");
+    }
+    console.log("Your Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+    alert("Your Score: " + humanScore);
+    alert("Computer Score: " + computerScore);
+}
